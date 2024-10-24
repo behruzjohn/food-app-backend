7ea45571935b4784b364699e27dcd8720a59954b
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-10-24 09:33:57 -0700

    built schemas

--- src/enums/status.enum.ts
+++ src/enums/status.enum.ts
@@ -1,4 +1,5 @@
 export enum StatusEnum {
   received = "received",
   cooking = "cooking",
+  on_the_way = "on_the_way",
 }
--- src/modules/cart/cart.type.ts
+++ src/modules/cart/cart.type.ts
@@ -4,8 +4,10 @@ export const cartTypes = gql`
   enum OrderStatusEnum {
     received
     cooking
+    on_the_way
   }
   type Cart {
+    _id: ID
     price: Number
     createdAt: Date
     status: OrderStatusEnum
