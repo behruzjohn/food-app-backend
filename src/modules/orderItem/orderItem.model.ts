9daffc7b889025f935f0ffbca9ba504ea0f9245a
fazliddinquvatboyev8@gmail.com <fazliddinquvatboyev8@gmail.com>
2024-11-22 12:10:21 +0500

    fix: issues

---
+++ src/modules/orderItem/orderItem.model.ts
@@ -0,0 +1,14 @@
+import { model, Schema, Types } from 'mongoose';
+import { MODELS } from '../../constants/models';
+
+const orderItemSchema = new Schema(
+  {
+    food: { type: Types.ObjectId, ref: MODELS.FOOD },
+    user: { type: Types.ObjectId, ref: MODELS.USER },
+    price: { type: Number },
+    quantity: { type: Number, default: 1, min: 1 },
+  },
+  { timestamps: true },
+);
+
+export const OrderItem = model(MODELS., orderItemSchema);
--- src/types/context.ts
+++ src/types/context.ts
@@ -1,5 +1,6 @@
+import { Types } from 'mongoose';
 import { User } from 'src/modules/user/user.model';
 
 export type Context = {
-  user: typeof User.schema.obj;
+  user: typeof User.schema.obj & { _id: Types.ObjectId };
 };
