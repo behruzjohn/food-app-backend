ba00b675618158b61bcb4dc5fbead1f0b26833b5
fazliddinquvatboyev8@gmail.com <fazliddinquvatboyev8@gmail.com>
2024-11-22 20:02:01 +0500

    fix: add secret key to env

--- src/sms/index.ts
+++ src/sms/index.ts
@@ -5,7 +5,7 @@ export const sendSms = async (number: string) => {
   const code = Math.floor(Math.random() * 90000) + 10000;
 
   const data = {
-    secret: '88e27bf9623f17c0efc3530b8fcce5ffef4f4e87',
+    secret: process.env.SMS_SECRET_KEY,
     phone: `${number}`,
     mode: 'devices',
     device: '00000000-0000-0000-bf9a-a7b684c06c8d',
