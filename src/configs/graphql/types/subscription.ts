e278f0cdaac38e2a8c23bff0c6b913bb01fac207
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2024-12-10 14:23:16 +0500

    update: subscriptions schema

--- src/configs/graphql/types/subscription.ts
+++ src/configs/graphql/types/subscription.ts
@@ -3,12 +3,12 @@ import { SUBSCRIPTIONS } from 'src/constants/subscriptions';
 
 export const subscriptionType = gql`
   type Subscription {
-    ${SUBSCRIPTIONS.UPDATE_ORDER_STATUS_BY_ID}(order: OrderStatusUpdateInput!): OrderOutput
-    ${SUBSCRIPTIONS.DELIVER_ORDER_BY_ID}(orderId: ID!): FoodOutput
-    ${SUBSCRIPTIONS.RECEIVE_ORDER_BY_ID}(orderId: ID!): FoodOutput
-    ${SUBSCRIPTIONS.START_COOKING_FOOD}(orderId: ID!): OrderOutput
-    ${SUBSCRIPTIONS.CREATE_ORDER}(order: OrderInput!): OrderOutput
-    ${SUBSCRIPTIONS.ATTACH_ORDER}(orderId: ID!): CourierOutput
-    ${SUBSCRIPTIONS.ATTACH_ORDER_TO_COURIER}(orderId: ID!, courierId: ID!): CourierOutput
+    ${SUBSCRIPTIONS.UPDATE_ORDER_STATUS_BY_ID}: OrderOutput
+    ${SUBSCRIPTIONS.DELIVER_ORDER_BY_ID}: FoodOutput
+    ${SUBSCRIPTIONS.RECEIVE_ORDER_BY_ID}: FoodOutput
+    ${SUBSCRIPTIONS.START_COOKING_FOOD}: OrderOutput
+    ${SUBSCRIPTIONS.CREATE_ORDER}: OrderOutput
+    ${SUBSCRIPTIONS.ATTACH_ORDER}: CourierOutput
+    ${SUBSCRIPTIONS.ATTACH_ORDER_TO_COURIER}: CourierOutput
   }
 `;
--- src/types/context.ts
+++ src/types/context.ts
@@ -2,7 +2,7 @@ import { Types } from 'mongoose';
 import { UserSchema } from 'src/modules/user/types/userSchema.type';
 
 export type Context = {
-  user: UserSchema & { _id: typeof Types.ObjectId } & {
+  user: UserSchema & { _id: Types.ObjectId } & {
     save: () => Promise<void>;
   };
 };
