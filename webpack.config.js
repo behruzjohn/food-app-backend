87bb6bfdbfb7e2ee6823c13bce90087afb35c1b6
behruzjon <baxtiyorovbehruzjon6789@gmail.com>
2025-12-11 19:51:45 +0500

    fixed bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const webpack = require("webpack");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path');
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: "./src/index.ts",
-  target: "node",
-  mode: "production",
+  entry: './src/index.ts',
+  target: 'node',
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
-    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   externals: [nodeExternals()],
   plugins: [
