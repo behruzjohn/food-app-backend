deaf721c39a09886b7220342e158bf825adde940
behruzjon <baxtiyorovbehruzjon6789@gmail.com>
2025-12-14 14:08:41 +0500

    changed code

--- src/services/sms/index.ts
+++ src/services/sms/index.ts
@@ -1,38 +1,8 @@
-import bcrypt from 'bcrypt';
-import { PHONE_CONFIRMATION_CODE_LENGTH } from 'src/constants/auth';
-import { logger } from 'src/services/logger';
-import { generateRandomNumbers } from 'src/utils/crypto';
-
 export const sendSms = async (phone: string) => {
-  try {
-    const code = generateRandomNumbers(PHONE_CONFIRMATION_CODE_LENGTH);
+  const code = '12345';
 
-    console.log('code', code);
+  console.log(`📩 FIXED SMS CODE for ${phone}: ${code}`);
 
-    // const data = {
-    //   secret: '88e27bf9623f17c0efc3530b8fcce5ffef4f4e87',
-    //   phone: `${number}`,
-    //   mode: 'devices',
-    //   device: '00000000-0000-0000-bf9a-a7b684c06c8d',
-    //   sim: 1,
-    //   priority: 1,
-    //   message: `Ro'yhatdan o'tish codi: ${code}`,
-    // };
-
-    // const envVariable = 'SMS_CENTER_SINGLE_SMS_URL';
-
-    // await axios.post(process.env[envVariable], data, {
-    //   headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //   },
-    // });
-    // console.log(res, process.env[envVariable]);
-
-    const hash = await bcrypt.hash(code + '', 10);
-
-    return hash;
-  } catch (error) {
-    logger.error('SMS Sender', error);
-  }
+  const hash = await bcrypt.hash(code, 10);
+  return hash;
 };
